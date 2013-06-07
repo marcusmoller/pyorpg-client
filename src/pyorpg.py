6636f76d6685908e0df2d65a8ef9883bfc86bc1f
marcusmoller <marcus@powrtoch.org>
2013-06-07 17:56:38 +0200

    Added basic argument parsing

--- src/pyorpg.py
+++ src/pyorpg.py
@@ -3,16 +3,24 @@
 
 import sys
 import getopt
+import argparse
 import pygame
 from pygame.locals import *
 
+import engine
 import global_vars as g
 from constants import *
 
+parser = argparse.ArgumentParser(prog='PyORPG')
+parser.add_argument('-ip', help='connect to server on ip')
+parser.add_argument('-p', help='connect to server on port')
+args = vars(parser.parse_args())
+
+if args['ip'] != None:
+	print "ip todo"
+
 pygame.display.set_caption(GAME_NAME)
 g.screenSurface = pygame.display.set_mode((g.SCREEN_WIDTH, g.SCREEN_HEIGHT))
-
-import engine
 
 if __name__ == "__main__":
     g.gameEngine = engine.Engine()
