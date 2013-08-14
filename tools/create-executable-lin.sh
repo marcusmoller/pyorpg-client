43b69910839e23b0e76834083f869e4ba622eef5
marcusmoller <marcus@powrtoch.org>
2013-08-14 13:45:57 +0200

    Added initial script for generating executables on GNU/Linux

---
+++ tools/create-executable-lin.sh
@@ -0,0 +1,16 @@
+echo "--------------------------------------------------------"
+echo "PyORPG Executable Tool for GNU/Linux (using PyInstaller)"
+echo "--------------------------------------------------------"
+
+read -p "Choose a name for your executable: " execname
+
+echo "Building executable using PyInstaller..."
+pyinstaller ../src/pyorpg.py -F -o ../tmp/ -n $execname
+
+echo "Making bin folder if it doesnt exist..."
+mkdir ../bin
+
+echo "Moving binary into the bin folder..."
+mv ../tmp/dist/$execname ../bin/
+
+echo "Generating launcher for executable..."
