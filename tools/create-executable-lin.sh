c0c79a8ef85e4bd80602fa1591b51ea20684e666
marcusmoller <marcus@powrtoch.org>
2013-08-15 02:18:38 +0200

    Improved the automatic executable creator

--- tools/create-executable-lin.sh
+++ tools/create-executable-lin.sh
@@ -14,3 +14,14 @@ echo "Moving binary into the bin folder..."
 mv ../tmp/dist/$execname ../bin/
 
 echo "Generating launcher for executable..."
+echo '#!/bin/bash' >> ../$execname-lin
+echo '# Generated launcher script for PyORPG' >> ../$execname-lin
+echo "cd bin" >> ../$execname-lin
+echo "./$execname" >> ../$execname-lin
+
+echo "Setting permissions for launcher..."
+chmod +x ../$execname-lin
+
+echo "Done!"
+echo "--------------------------------------------------------"
+echo "Compress the bin-folder, data-folder and $execname-lin to share the game online."
